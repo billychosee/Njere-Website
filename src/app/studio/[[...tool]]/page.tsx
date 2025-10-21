a2c7c4a83ce9ddc0978b76746a5602eb9ccfb838
Billy Kadyamatimba <billy@smatechgroup.com>
2025-10-21 08:06:45 +0200

    Fix Sanity Studio createContext error by adding 'use client' directive

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,14 +7,12 @@
  * https://github.com/sanity-io/next-sanity
  */
 
+'use client'
+
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
 export const dynamic = 'force-dynamic';
-
-
-
-export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
